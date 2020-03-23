940fea7183eb92a0183ed45a968e0b11391258b2
Confusingboat <confusingboat@gmail.com>
2020-03-23 01:40:45 -0500

    Update check-health.sh

--- check-health.sh
+++ check-health.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 STATUS=$(curl -I -s -o /dev/null -w '%{http_code}' $PLEX_HEALTH_URL)
-if [ $STATUS -ge 200 ] && [ $STATUS -lt 400 ] && [ ls "$PLEX_HEALTH_MEDIA_DIR" ]; then
-  return 0
-else
-  return 1
+if [ $STATUS -lt 200 ] || [ $STATUS -ge 400 ]; then
+  echo "Invalid HTTP status $STATUS" && return 1
 fi
+
+if [ ! ls "$PLEX_HEALTH_MEDIA_DIR" ]; then
+  echo "Unable to list media dir" && return 1
+fi
+
+echo "Health check success" && return 0
