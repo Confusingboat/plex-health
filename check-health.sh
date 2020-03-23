9777f35c1da06c20035a2fbe489bae54f56f89c1
Confusingboat <confusingboat@gmail.com>
2020-03-23 01:01:00 -0500

    Update check-health.sh

--- check-health.sh
+++ check-health.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-ls "$PLEX_MEDIA_DIR" && curl -I $PLEX_PROTO://$PLEX_HOST:$PLEX_PORT/web/index
+STATUS=$(curl -I -s -o /dev/null -w '%{http_code}' $PLEX_HEALTH_URL)
+if [ $STATUS -ge 200 && $STATUS -lt 400 && ls "$PLEX_HEALTH_MEDIA_DIR" ]; then
+  return 0
+else
+  return 1
+fi
